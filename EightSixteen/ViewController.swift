52cc818955520d422758055ab0004d8d9e71ca5f
peppermint100 <krpeppermint100@gmail.com>
2024-02-17 18:29:07 +0900

    [Build] 스토리보드 제거

---
+++ EightSixteen/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EightSixteen
+//
+//  Created by peppermint100 on 2/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
