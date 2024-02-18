f505bcef067601e7985ddf7ddeac110d8b18c682
peppermint100 <krpeppermint100@gmail.com>
2024-02-18 13:51:14 +0900

    [Feat] TodayIndicator를 통한 날짜 표시

---
+++ EightSixteen/View/TodayIndicatorView.swift
@@ -0,0 +1,76 @@
+//
+//  TodayIndicatorView.swift
+//  EightSixteen
+//
+//  Created by peppermint100 on 2/18/24.
+//
+
+import UIKit
+import RxSwift
+
+class TodayIndicatorView: UIStackView {
+    
+    private let monthLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 36, weight: .bold)
+        label.numberOfLines = 1
+        label.textColor = .label
+        return label
+    }()
+    
+    private let dayLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 36, weight: .bold)
+        label.numberOfLines = 1
+        label.textColor = .label
+        return label
+    }()
+    
+    private let dayOfWeekLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 26, weight: .bold)
+        label.numberOfLines = 1
+        label.textColor = .label
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        axis = .vertical
+        distribution = .fillEqually
+        addArrangedSubview(monthLabel)
+        addArrangedSubview(dayLabel)
+        addArrangedSubview(dayOfWeekLabel)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+    
+    func bindDate(dateObservable: Observable<Date>) {
+        let disposeBag = DisposeBag()
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        
+        dateObservable.map { date in
+            let month = calendar.component(.month, from: date)
+            return "\(month)"
+        }.bind(to: monthLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        dateObservable.map { date in
+            let day = calendar.component(.day, from: date)
+            return "\(day)"
+        }.bind(to: dayLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        dateObservable.map { date in
+            let weekDay = calendar.component(.weekday, from: date)
+            return "\(dateFormatter.weekdaySymbols[weekDay-1])"
+        }.bind(to: dayOfWeekLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+}
