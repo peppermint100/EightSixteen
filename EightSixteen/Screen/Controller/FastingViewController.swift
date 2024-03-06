469694c716e3fb11a61c3dbccb4a05d3a1ce47d1
peppermint100 <krpeppermint100@gmail.com>
2024-03-06 11:44:34 +0900

    [Chore]

---
+++ EightSixteen/Screen/Controller/FastingViewController.swift
@@ -0,0 +1,144 @@
+//
+//  FastingViewController.swift
+//  EightSixteen
+//
+//  Created by peppermint100 on 2/17/24.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class FastingViewController: UIViewController {
+    
+    var viewModel: FastingViewModel!
+    private let disposeBag = DisposeBag()
+    
+    private var todayIndicator = TodayIndicatorView()
+    private var fastingCountView = FastingCountView()
+    private var fastingCircleView = CircleGaugeView()
+    private var emptyFastingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "현재 진행중인 단식이 없습니다."
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private var endFastingButton = ESAppButton(title: "단식 종료하기")
+    private var startFastingButton = ESAppButton(title: "단식 시작하기")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupTodayIndicator()
+        bindViewModel()
+    }
+    
+    func bindViewModel() {
+        let barButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItems = [barButtonItem]
+        
+        let input = FastingViewModel.Input(
+            startFastingButtonTapped: startFastingButton.rx.tap.asObservable(),
+            endFastingButtonTapped: endFastingButton.rx.tap.asObservable(),
+            barButtonItemTapped: barButtonItem.rx.tap.asObservable()
+        )
+        
+        let output = viewModel.transform(input)
+        
+        output.showFasting
+            .bind(to: emptyFastingLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        output.showFasting
+            .map { !$0 }
+            .bind(to: fastingCircleView.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        output.showFasting
+            .map { !$0 }
+            .bind(to: fastingCountView.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        output.showFasting
+            .map { $0 }
+            .bind(to: startFastingButton.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        output.showFasting
+            .map { !$0 }
+            .bind(to: endFastingButton.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        output.fasting
+            .map { $0.fastingRatio }
+            .bind(onNext: { [weak self] in
+                self?.fastingCircleView.greenPathOccupationRatio = $0
+                self?.fastingCircleView.setNeedsDisplay()
+            })
+            .disposed(by: disposeBag)
+        
+        output.fasting
+            .map { $0.fastingDayCount }
+            .map { "\($0)일째 단식 중" }
+            .bind(to: fastingCountView.fastingCountLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.fastingStatusIndicatorText
+            .bind(to: fastingCircleView.timerLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(todayIndicator)
+        view.addSubview(fastingCountView)
+        view.addSubview(fastingCircleView)
+        view.addSubview(startFastingButton)
+        view.addSubview(endFastingButton)
+        view.addSubview(emptyFastingLabel)
+        
+        todayIndicator.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(140)
+        }
+        
+        fastingCountView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(40)
+            make.top.equalTo(todayIndicator.snp.bottom).offset(30)
+            make.height.equalTo(70)
+        }
+        
+        fastingCircleView.snp.makeConstraints { make in
+            make.top.equalTo(fastingCountView.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.bottom.equalTo(startFastingButton.snp.top)
+        }
+        
+        emptyFastingLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.equalTo(todayIndicator.snp.bottom)
+            make.bottom.equalTo(startFastingButton.snp.top)
+        }
+        
+        startFastingButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(40)
+            make.height.equalTo(60)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
+        }
+        
+        endFastingButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(40)
+            make.height.equalTo(60)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
+        }
+    }
+    
+    private func setupTodayIndicator() {
+        self.todayIndicator.today = Date()
+    }
+}
---
+++ EightSixteen/ViewModel/FastingViewModel.swift
@@ -0,0 +1,205 @@
+//
+//  FastingViewModel.swift
+//  EightSixteen
+//
+//  Created by peppermint100 on 2/18/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import Action
+import UserNotifications
+
+class FastingViewModel {
+    
+    var coordinator: FastingCoordinator?
+    private var disposeBag = DisposeBag()
+    private var timer: DispatchSourceTimer?
+    
+    struct Input {
+        let startFastingButtonTapped: Observable<Void>
+        let endFastingButtonTapped: Observable<Void>
+        let barButtonItemTapped: Observable<Void>
+    }
+    
+    struct Output {
+        let showFasting: BehaviorSubject<Bool>
+        let fasting: BehaviorSubject<Fasting>
+        let fastingStatusIndicatorText: BehaviorSubject<String>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        checkNotificationPermission()
+        
+        let fasting = FastingManager.shared.load()
+        let showFastingObservable = BehaviorSubject(value: fasting != nil)
+        let fastingObservable: BehaviorSubject<Fasting>
+        let timerSeconds: BehaviorSubject<TimeInterval>
+        let fastingStatusIndicatorText = BehaviorSubject(value: "")
+        
+        if let fasting = fasting {
+            fastingObservable = BehaviorSubject(value: fasting)
+            timerSeconds = BehaviorSubject(value: fasting.fastingTimeRemaining)
+        }
+        else {
+            let fasting = Fasting()
+            fastingObservable = BehaviorSubject(value: fasting)
+            timerSeconds = BehaviorSubject(value: TimeInterval(Fasting.defaultFastingHours * 3600))
+        }
+        
+        configureBarButtonItem(input.barButtonItemTapped, fastingObservable, showFasting: showFastingObservable)
+        configureStartFastingButtonTapped(input.startFastingButtonTapped, showFastingObservable, fastingSubject: fastingObservable, timerSeconds: timerSeconds)
+        configureEndFastingButton(input.endFastingButtonTapped, showFasting: showFastingObservable)
+        configureFastingStatusIndicatorText(timerSeconds: timerSeconds, text: fastingStatusIndicatorText)
+        configureTimer(showFastingObservable, timerSeconds: timerSeconds)
+        
+        let output = FastingViewModel.Output(
+            showFasting: showFastingObservable,
+            fasting: fastingObservable,
+            fastingStatusIndicatorText: fastingStatusIndicatorText
+        )
+        
+        return output
+    }
+    
+    private func configureFastingStatusIndicatorText(timerSeconds: BehaviorSubject<TimeInterval>, text: BehaviorSubject<String>) {
+        timerSeconds
+            .subscribe(onNext: {
+                if $0 > 0 {
+                    text.onNext($0.formatTimeIntervalToHHMMSS())
+                } else {
+                    text.onNext("식사 시간입니다.")
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureTimer(_ showFasting: BehaviorSubject<Bool>, timerSeconds: BehaviorSubject<TimeInterval>) {
+        showFasting.subscribe(onNext: { [weak self] showFasting in
+            if self?.timer != nil {
+                self?.timer?.cancel()
+                self?.timer = nil
+            }
+            
+            if showFasting {
+                guard var seconds = try? timerSeconds.value() else { return }
+                self?.timer = DispatchSource.makeTimerSource(queue: DispatchQueue.global(qos: .background))
+                self?.timer?.schedule(deadline: .now(), repeating: 1)
+                self?.timer?.setEventHandler {
+                    if seconds < 0 {
+                        self?.timer?.cancel()
+                        self?.timer = nil
+                    }
+                    seconds -= 1
+                    timerSeconds.onNext(seconds)
+                }
+                self?.timer?.resume()
+            }
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    private func configureBarButtonItem(_ tapped: Observable<Void>, _ fasting: Observable<Fasting>, showFasting: BehaviorSubject<Bool>) {
+        tapped.subscribe(onNext: { [weak self] _ in
+            var fasting = FastingManager.shared.load()
+            print(fasting?.startedAt.addingTimeInterval(3600*9))
+            print("endedAt= ", fasting?.endedAt.addingTimeInterval(3600 * 9))
+            print("hours = ", fasting?.fastingTimeHours)
+            print("ratio = ", fasting?.fastingRatio)
+            print("remaning = ", fasting?.fastingTimeRemaining)
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    private func configureStartFastingButtonTapped(
+        _ tapped: Observable<Void>, _ showFasting: BehaviorSubject<Bool>,
+        fastingSubject: BehaviorSubject<Fasting>, timerSeconds: BehaviorSubject<TimeInterval>) {
+        tapped.subscribe(onNext: { [weak self] in
+            let fasting = Fasting()
+            self?.coordinator?.presentFastingComposeVC(fasting: fasting) { fastingCreated, fasting in
+                timerSeconds.onNext(fasting.fastingTimeRemaining)
+                showFasting.onNext(fastingCreated)
+                fastingSubject.onNext(fasting)
+                self?.registerNotification(fasting)
+            }
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    private func configureEndFastingButton(_ tapped: Observable<Void>, showFasting: BehaviorSubject<Bool>) {
+        tapped
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.alertByEndFastingButton(onOk: { _ in
+                    self?.endFasting(showFasting)
+                })
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func endFasting(_ showFasting: BehaviorSubject<Bool>) {
+        let notificationCenter = UNUserNotificationCenter.current()
+        timer?.cancel()
+        timer = nil
+        FastingManager.shared.clear()
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: [NotificationKey.fastingStart.rawValue, NotificationKey.fastingEnd.rawValue])
+        showFasting.onNext(false)
+    }
+    
+    private func checkNotificationPermission() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized:
+                return
+            case .notDetermined:
+                notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { allowed, error in
+                    guard error == nil else {
+                        print("Error occured")
+                        return
+                    }
+                }
+                return
+            case .denied:
+                return
+            default:
+                print("Default")
+                return
+            }
+        }
+    }
+    
+    private func registerNotification(_ fasting: Fasting) {
+        checkNotificationPermission()
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        let calendar = Calendar.current
+        let startTitle = "단식이 시작되었습니다."
+        let startBody = "단식을 유지하되 무리하지 마세요!"
+        let startDateComponent = calendar.dateComponents([.hour, .minute], from: fasting.startedAt)
+        
+        let endTitle = "단식이 종료되었습니다."
+        let endBody = "식사 맛있게 하세요!"
+        let endDateComponent = calendar.dateComponents([.hour, .minute], from: fasting.endedAt)
+        
+        let startContent = UNMutableNotificationContent()
+        startContent.title = startTitle
+        startContent.body = startBody
+        startContent.sound = .default
+        
+        let endContent = UNMutableNotificationContent()
+        endContent.title = endTitle
+        endContent.body = endBody
+        endContent.sound = .default
+        
+        let startTrigger = UNCalendarNotificationTrigger(dateMatching: startDateComponent, repeats: true)
+        let endTrigger = UNCalendarNotificationTrigger(dateMatching: endDateComponent, repeats: true)
+        
+        let startRequest = UNNotificationRequest(identifier: NotificationKey.fastingStart.rawValue, content: startContent, trigger: startTrigger)
+        let endRequest = UNNotificationRequest(identifier: NotificationKey.fastingEnd.rawValue, content: endContent, trigger: endTrigger)
+        
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: [NotificationKey.fastingStart.rawValue, NotificationKey.fastingEnd.rawValue])
+        notificationCenter.add(startRequest)
+        notificationCenter.add(endRequest)
+    }
+}
